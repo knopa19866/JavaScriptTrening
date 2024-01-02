8c69cf4b56ce3d810b966aa360ca2c352c2aaf7a
Anna Linenko <misija@mail.ru>
2024-01-02 14:33:35 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+  a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+const answers = [];
+
+answers[0] = prompt('как Вас зовут?', '');
+answers[1] = prompt('с какакого Вы Города ', '');
+answers[2] = prompt('сколько Вам лет', '');
+
